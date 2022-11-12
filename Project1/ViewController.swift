032bc062dbef06aed4763de82c1f4dbe22ed6c11
Bakhtiyarov Fozilkhon <bakhtiyarovfozilkhon@Bakhtiyarovs-MacBook-Air.local>
2022-11-12 13:07:16 +0600

    Initial commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Bakhtiyarov Fozilkhon on 12.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
